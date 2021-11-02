95ed9e55c7eee38eff03d4f5f0ce4795787f7f28
ShadowDead21 <83669275+ShadowDead21@users.noreply.github.com>
2021-11-02 17:27:40 -0300

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+require('./db/conexionDb.php');
+
+if(isset($_SESSION['id_user']) && isset($_SESSION['id_rol'])){
+    $sql = 'SELECT idrol from roles where idrol in(SELECT rol from login where
+     idlog ='.$_SESSION['id_user'].')';
+    $resultado = mysqli_query($conexion, $sql);
+    if (!empty($resultado) && mysqli_num_rows($resultado) != 0){
+        $row = mysqli_fetch_assoc($resultado);
+    }
+    
+    // Compruebo de que exista
+    if(isset($row['idrol'])){
+        if($row['idrol'] == 1){
+            header('location: /bolsa-empleo-php/iaes-lgi-2021/vistas/dashboardAdmin.php');
+        } else if($row['idrol'] == 2){
+            header('location: /bolsa-empleo-php/iaes-lgi-2021/vistas/dashboardSecretaria.php');
+        } else if($row['idrol'] == 3){
+            header('location: /bolsa-empleo-php/iaes-lgi-2021/vistas/dashboardUser.php');
+        }
+    }
+    mysqli_close($conexion);
+
+}else{
+    header('location: /bolsa-empleo-php/iaes-lgi-2021/index.php');
+}
+
+
+
+
+
+
+
+
+
+?>

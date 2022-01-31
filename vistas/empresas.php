0fe846a350edccccea20d918cb3479fde98eadad
ShadowDead21 <83669275+ShadowDead21@users.noreply.github.com>
2022-01-31 20:15:02 -0300

    Add files via upload

--- vistas/empresas.php
+++ vistas/empresas.php
@@ -1,91 +1,102 @@
 <?php
 require('../db/conexionDb.php');
-require('../db/verificarCredenciales.php');
 ?>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Empresas</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../plugins/sweetalert/sweetalert2.min.css">
     <link rel="stylesheet" href="../assets/css/register.css">
-    <link rel="stylesheet" href="../assets/css/filtro.css">
-  
+    <link rel="stylesheet" href="../assets/css/filtro.css">  
+    <script src="../jquery/jquery-3.6.0.min.js"></script>
+    <script src="../popper/popper.min.js" type="text/javascript"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../plugins/sweetalert/sweetalert2.all.min.js"></script>
+    <script src="../assets/js/createUser.js"></script>
+    <script src="../assets/js/empresasjs.js"></script>
 </head>
 <body>
-    <div class="content">
-        <div id="log_img" class="logo">
-            <a href="#" class="logo__link">
-            <img
-                src="http://www.iaes.edu.ar/wp-content/uploads/2014/08/logo-top-1.png"
-                alt="Logo del IAES"
-            />
-            </a>
-        </div>
-        <div class="create">
-            <a id="botonCrear" href="./registro.php">Crear Usuario</a>
-        </div>
-        <header id="header" class="header_dasboard">
-            <a class="header_link" href="./editarCredenciales.php">
-                <?php echo $_SESSION['usuario'];?></a>
-            <a class="header_link" href="filtro.php">Filtro</a>
-            <a class="header_link" href="../db/logout.php">Salir</a>
-        </header>
-    </div>
-    <div class="row">
-        <div class="col 3">
-            <h1 class="text-decoration-underline">Registrar Empresas</h1><br>
-            <form id="empresas" method="post">
-                <div class="input-field">
-                    <label for="empresa">Empresa</label>
-                    <input  type="text" name="empresa" value="" id="empresa" placeholder="">
-                    
-                </div>
-                
-                <div class="input-field">
-                    <label for="cuit">CUIT</label>
-                    <input  type="text" name="cuit" value="" id="cuit" placeholder="">
-                   
-                </div>                
-                <div class="input-field">
-                    <label for="presidente">Presidente</label>
-                    <input  type="text" name="presidente" value="" id="presidente" placeholder="">
-                    
-                </div>
-                
-                <div class="input-field">
-                    <label for="correo">Correo</label>
-                    <input  type="text" name="correo" value="" id="correo" placeholder="">
-                </div>
-                
-                <div class="input-field">
-                    <label for="telefono">Telefono</label>
-                    <input  type="text" name="telefono" value="" id="telefono" placeholder="">
-                </div>
-                 <div class="input-field">
-                     <button  type="submit" name="guardar" class="btn-success">Guardar</button>
-                    
-                </div>
-                
-            </form>
-        </div>
+    <header>
+        <h3 class="text-center">Listado de Empresas</h3>        
+    </header>
+    <div class="container">
+        <div id="tabla"></div>
     </div>
 
-
-<script src="../jquery/jquery-3.6.0.min.js"></script>
-<script src="../bootstrap/js/bootstrap.min.js"></script>
-<script src="../popper/popper.min.js"></script>
-<script src="../plugins/sweetalert/sweetalert2.all.min.js"></script>
-<script src="../assets/js/empresasjs.js"></script>
-
-<!--<script>
-    $(document).ready(function(){
-        M.AutoInit();
-    });
-    $(document).ready(function(){
-        
-    });
-</script>-->
+    <!-- Modal -->
+    <div class="modal fade" id="modalagregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Agregar Empresa</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <label>Empresa</label>
+                <input type="text" name="empresa" id="empresa" class="form-control input-sm">
+                <label>CUIT</label>
+                <input type="text" name="cuit" id="cuit" class="form-control input-sm">
+                <label>Presidente</label>
+                <input type="text" name="presidente" id="presidente" class="form-control input-sm">
+                <label>Correo</label>
+                <input type="text" name="correo" id="correo" class="form-control input-sm"> 
+                <label>Telefono</label>
+                <input type="text" name="telefono" id="telefono" class="form-control input-sm"> 
+            </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="guardarN">Guardar</button>
+          </div>
+        </div>
+      </div>
+    </div>
 </body>
+
+<div class="modal fade" id="modaleditar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Editar Informacion de Empresa</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+                <label>Empresa</label>
+                <input type="text" name="empresae" id="empresae" class="form-control input-sm">
+                <label>CUIT</label>
+                <input type="text" name="cuite" id="cuite" class="form-control input-sm">
+                <label>Presidente</label>
+                <input type="text" name="presidentee" id="presidentee" class="form-control input-sm">
+                <label>Correo</label>
+                <input type="text" name="correoe" id="correoe" class="form-control input-sm"> 
+                <label>Telefono</label>
+                <input type="text" name="telefonoe" id="telefonoe" class="form-control input-sm"> 
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
+<script type="text/javascript">
+    $(document).ready(function(){
+       $('#tabla').load('empresastabla.php');
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function(){
+       $('#guardarN').click(function(){
+           empresa=$('#empresa').val();
+           cuit=$('#cuit').val();
+           presidente=$('#presidente').val();
+           correo=$('#correo').val();
+           telefono=$('#telefono').val();
+           agregardatos(empresa,cuit,presidente,correo,telefono);
+       }); 
+    });
+</script>
---
+++ vistas/empresastabla.php
@@ -0,0 +1,46 @@
+<?php
+require '../db/conexionDb.php';
+?>
+
+<div class="row">
+    <div class="col-sm-12">
+        <table class="table table-bordered">
+            <caption>
+                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalagregar">Agregar                 
+                </button>
+            </caption>
+            
+            <tr>
+                <td>Empresa</td>
+                <td>Cuit</td>
+                <td>Presidente</td>
+                <td>Correo</td>
+                <td>Telefono</td>
+                <td>Editar</td>
+                <td>Eliminar</td>
+            </tr>
+            <?php
+            
+                $sql = "SELECT `idempresa`, `empresa`, `cuit`, `presidente`, `correo`, `telefono` FROM `empresas` WHERE 1";
+		$lista = mysqli_query($conexion, $sql);
+                while ($fila = $lista->fetch_assoc()) {
+            ?>
+            <tr>
+                <td><?php echo $fila['empresa']?></td>
+                <td><?php echo $fila['cuit']?></td> 
+                <td><?php echo $fila['presidente']?></td> 
+                <td><?php echo $fila['correo']?></td> 
+                <td><?php echo $fila['telefono']?></td> 
+                <td>
+                    <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modaleditar">Editar</button>
+                </td>  
+                <td>
+                    <button class="btn btn-danger">Eliminar</button>
+                </td>
+             <?php
+                }
+             ?>
+            </tr>
+        </table>        
+    </div>
+</div>

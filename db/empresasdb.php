daf679f5d9a74c5098bdbc844cf9da05ebf09892
ShadowDead21 <83669275+ShadowDead21@users.noreply.github.com>
2022-01-31 20:15:33 -0300

    Add files via upload

--- db/empresasdb.php
+++ db/empresasdb.php
@@ -1,5 +1,5 @@
 <?php
-require('./conexionDb.php');
+require('conexionDb.php');
 $empresa=$_POST['empresa'];
 $cuit=$_POST['cuit'];
 $presidente=$_POST['presidente'];
@@ -7,12 +7,13 @@ $correo=$_POST['correo'];
 $telefono=$_POST['telefono'];
 
 
-$sql= "INSERT INTO `empresas`(`empresa`, `cuit`, `presidente`, `correo`, `telefono`) VALUES ('".$empresa."','".$cuit."','".$presidente."','".$correo."','".$telefono."');";
+$sql= "INSERT INTO `empresas`(`empresa`, `cuit`, `presidente`, `correo`, `telefono`) VALUES ('".$empresa."','".$cuit."','".$presidente."','".$correo."','".$telefono."')";
 if (mysqli_query($conexion, $sql)) {
         $data = array('empresa'=>$empresa);
     }else{
         $data = false;
     }
-print json_encode($data);
+echo $result= mysql_query($conexion,$sql);
+
 mysqli_close($conexion);
 ?>

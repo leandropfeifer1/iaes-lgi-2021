06deb09de726125a418471d08e298d8e0a2f69ce
ShadowDead21 <83669275+ShadowDead21@users.noreply.github.com>
2022-03-08 15:34:47 -0300

    Update conexionDb.php

--- db/conexionDb.php
+++ db/conexionDb.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "proyecto2021";
+$servername = "https://iaes.edu.ar/phpmyadmin/";
+$username = "lgi21@iaes.edu.ar";
+$password = "lwBx0c4dPEpKsgA";
+$db = "iaes_lgi21";
 
 $conexion = mysqli_connect($servername, $username, $password, $db);
 

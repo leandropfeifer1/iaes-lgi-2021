4d33a94e349fdbdef28e3d3b04a18bb90bb637a8
JavierPineyro <javidunam22@gmail.com>
2022-03-25 19:04:00 -0300

    Cambios redireccionamientos

--- db/conexionDb.php
+++ db/conexionDb.php
@@ -1,8 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "proyecto2021";
+// $servername = "localhost";
+$servername = "https://iaes.edu.ar/phpmyadmin";
+// $username = "root";
+$username = "lgi21@iaes.edu.ar";
+// $password = "";
+$password = "lwBx0c4dPEpKsgA";
+// $db = "proyecto2021";
+$db = "iaes_lgi21";
 
 $conexion = mysqli_connect($servername, $username, $password, $db);
 
--- db/logout.php
+++ db/logout.php
@@ -4,6 +4,6 @@ unset($_SESSION['id_user']);
 unset($_SESSION['usuario']);
 unset($_SESSION['id_rol']);
 session_destroy();
-//header('location: /index.php');
- header('location: /bolsa-empleo-php/iaes-lgi-2021/index.php');
+header('location: /index.php');
+//  header('location: /bolsa-empleo-php/iaes-lgi-2021/index.php');
 ?>

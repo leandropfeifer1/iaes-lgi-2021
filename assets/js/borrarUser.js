4f3336e5257cecbf08b6c11c71d69451630a4c0b
MBareiro <martinchoo_13@hotmail.com>
2022-05-20 18:43:22 -0300

    Agregado de cartel de advertencia al eliminar usuario en vistaUsuario.php

--- assets/js/borrarUser.js
+++ assets/js/borrarUser.js
@@ -1,18 +1,34 @@
 // ACE SE VA A BORRAR LOS USUARIOS DE vistaUsuario.php
 $("#eliminarUsuario").click(function () {
     var iduser = $("#idUser").val();
-    $.ajax({
-        type: "POST",
-        async: false,
-        url: "../db/eliminarUsuario.php",
-        datatype: 'json',
-        data: {
-            iduser: iduser,
-        },
-        success: function (response) {
-            console.log("uu");
-            var ventana = window.self;
-            ventana.close();
-        },
+
+    Swal.fire({
+        title: "Confirme",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si",
+        cancelButtonText: "No",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "POST",
+                async: false,
+                url: "../db/eliminarUsuario.php",
+                datatype: 'json',
+                data: {
+                    iduser: iduser,
+                },
+                success: function (response) {
+                    console.log("uu");
+                    var ventana = window.self;
+                    ventana.close();
+                },
+            });
+            
+        }
     });
+
+
 });

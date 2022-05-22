715cd4e0e9fdb7b24ee5ec2a166f473a319cde70
JavierPineyro <javidunam22@gmail.com>
2022-05-21 22:36:56 -0300

    Cambios relacion de aspecto de fotos, foto de usuario por defecto

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,90 +1,85 @@
 $('.div-datos').append(
   `<div class="errorMessage">No se encontraron Resultados</div>`
-);
-getData(); //trae todos los datos desde el inicio
-$('#carrera').change(() => sendRequest());
-$('#localidad').change(() => sendRequest());
-$('#licencia').change(() => sendRequest());
-$('#vehiculo').change(() => sendRequest());
-$('#edadMin').keyup(() => sendRequest());
-$('#edadMax').keyup(() => sendRequest());
-$('#edadMin').click(() => sendRequest());
-$('#edadMax').click(() => sendRequest());
-$('#modalidad').change(() => sendRequest());
-$('#genero').change(() => sendRequest());
-$('#disponible').change(() => sendRequest());
-$('#buscador').keyup(() => sendRequest());
+)
+getData() //trae todos los datos desde el inicio
+$('#carrera').change(() => sendRequest())
+$('#localidad').change(() => sendRequest())
+$('#licencia').change(() => sendRequest())
+$('#vehiculo').change(() => sendRequest())
+$('#edadMin').keyup(() => sendRequest())
+$('#edadMax').keyup(() => sendRequest())
+$('#edadMin').click(() => sendRequest())
+$('#edadMax').click(() => sendRequest())
+$('#modalidad').change(() => sendRequest())
+$('#genero').change(() => sendRequest())
+$('#disponible').change(() => sendRequest())
+$('#buscador').keyup(() => sendRequest())
 $('#reset').click(() => {
-  $('#carrera').val('');
-  $('#localidad').val('');
-  $('#licencia').val('');
-  $('#vehiculo').val('');
-  $('#edadMin').val('');
-  $('#edadMax').val('');
-  $('#modalidad').val('');
-  $('#genero').val('');
-  $('#disponible').val('');
-  $('#buscador').val('');
-  sendRequest();
-});
+  $('#carrera').val('')
+  $('#localidad').val('')
+  $('#licencia').val('')
+  $('#vehiculo').val('')
+  $('#edadMin').val('')
+  $('#edadMax').val('')
+  $('#modalidad').val('')
+  $('#genero').val('')
+  $('#disponible').val('')
+  $('#buscador').val('')
+  sendRequest()
+})
 
 function getData() {
-  $('.div-datos').empty();
+  $('.div-datos').empty()
   $.ajax({
     url: '../db/getDataStudents.php',
     type: 'POST',
     datatype: 'json',
     data: {},
     success: (data) => {
-      $('.div-datos').empty();
-      let disponibilidad = '';
-      let classAvailable = '';
-      let gender = '';
-      let tipoModalidad = '';
+      $('.div-datos').empty()
+      let disponibilidad = ''
+      let classAvailable = ''
+      let gender = ''
+      let tipoModalidad = ''
 
       if (data['data']) {
         data['data'].forEach((user) => {
           if (user.situacionlab && user.situacionlab === '1') {
-            disponibilidad = 'Disponible';
-            classAvailable = 'tag-purple';
+            disponibilidad = 'Disponible'
+            classAvailable = 'tag-purple'
           } else {
-            disponibilidad = 'No Disponible';
-            classNoAvailable = 'tag-red';
+            disponibilidad = 'No Disponible'
+            classNoAvailable = 'tag-red'
           }
 
           if (user.genero === '1') {
-            gender = 'Hombre';
+            gender = 'Hombre'
           } else if (user.genero === '2') {
-            gender = 'Mujer';
+            gender = 'Mujer'
           } else if (user.genero === '3') {
-            gender = 'No Binarix';
+            gender = 'No Binarix'
           } else {
-            gender = 'Otro';
+            gender = 'Otro'
           }
 
-          user.modalidad === '1'
-            ? (tipoModalidad = 'Full-Time')
-            : tipoModalidad;
+          user.modalidad === '1' ? (tipoModalidad = 'Full-Time') : tipoModalidad
 
-          user.modalidad === '2'
-            ? (tipoModalidad = 'Part-Time')
-            : tipoModalidad;
+          user.modalidad === '2' ? (tipoModalidad = 'Part-Time') : tipoModalidad
 
-          user.modalidad === '3' ? (tipoModalidad = 'Trainee') : tipoModalidad;
+          user.modalidad === '3' ? (tipoModalidad = 'Trainee') : tipoModalidad
 
-          user.modalidad === '4'
-            ? (tipoModalidad = 'Pasantías')
-            : tipoModalidad;
+          user.modalidad === '4' ? (tipoModalidad = 'Pasantías') : tipoModalidad
 
           if (user.foto) {
-            foto = '../db/images/' + user.foto;
+            foto = '../db/images/' + user.foto
           } else {
-            foto = '../assets/logo.jpg';
+            // foto = '../assets/logo.jpg'
+            foto = '../db/images/default.png'
           }
           $('.div-datos').append(
             `<a href="../vistas/vistaUsuario.php?iduser=${user.iduser}" target="_blank" class="card">
               <div class="card-header">
-                <img src="${foto}" alt="logo" />
+                <img style="object-fit: scale-down;" src="${foto}" alt="${user.usuario} ${user.apellido}" />
               </div>
               <div class="card-body">
                   <span class="tag ${classAvailable}">${disponibilidad}</span>
@@ -104,42 +99,42 @@ function getData() {
                   </div>
               </div>
           </a>`
-          );
-          classAvailable = '';
-        });
+          )
+          classAvailable = ''
+        })
       } else {
-        $('.div-datos').empty();
+        $('.div-datos').empty()
         $('.div-datos').append(`
           <div class="errorMessage">No se encontraron Resultados</div>
-        `);
+        `)
       }
     },
     error: (err) => {
-      console.log(err);
+      console.log(err)
     },
-  });
+  })
 }
 
 // Funcion para enviar Datos
 const sendRequest = () => {
-  $('.div-datos').empty();
-  const carrera = parseInt($('#carrera').val());
-  console.log(carrera);
-  const localidad = parseInt($('#localidad').val());
-  const licencia = parseInt($('#licencia').val());
-  const vehiculo = parseInt($('#vehiculo').val());
-  let edadMinima = $('#edadMin').val(); // Es "" si no se pone nada
-  let edadMaxima = $('#edadMax').val();
-  const modalidad = parseInt($('#modalidad').val());
-  const genero = parseInt($('#genero').val());
-  const disponible = parseInt($('#disponible').val());
-  let buscador = $('#buscador').val(); // Es "" si no se pone nada
+  $('.div-datos').empty()
+  const carrera = parseInt($('#carrera').val())
+  console.log(carrera)
+  const localidad = parseInt($('#localidad').val())
+  const licencia = parseInt($('#licencia').val())
+  const vehiculo = parseInt($('#vehiculo').val())
+  let edadMinima = $('#edadMin').val() // Es "" si no se pone nada
+  let edadMaxima = $('#edadMax').val()
+  const modalidad = parseInt($('#modalidad').val())
+  const genero = parseInt($('#genero').val())
+  const disponible = parseInt($('#disponible').val())
+  let buscador = $('#buscador').val() // Es "" si no se pone nada
 
-  edadMinima === '' ? (edadMinima = '1') : edadMinima;
-  edadMaxima === '' ? (edadMaxima = '1') : edadMaxima;
-  let edadMin = parseInt(edadMinima);
-  let edadMax = parseInt(edadMaxima);
-  edadMin >= edadMax ? (edadMax = 99) : edadMax;
+  edadMinima === '' ? (edadMinima = '1') : edadMinima
+  edadMaxima === '' ? (edadMaxima = '1') : edadMaxima
+  let edadMin = parseInt(edadMinima)
+  let edadMax = parseInt(edadMaxima)
+  edadMin >= edadMax ? (edadMax = 99) : edadMax
 
   $.ajax({
     url: '../db/filterData.php',
@@ -158,50 +153,44 @@ const sendRequest = () => {
       buscador: buscador,
     },
     success: (data) => {
-      $('.div-datos').empty();
-      let disponibilidad = '';
-      let classAvailable = '';
-      let gender = '';
-      let tipoModalidad = '';
+      $('.div-datos').empty()
+      let disponibilidad = ''
+      let classAvailable = ''
+      let gender = ''
+      let tipoModalidad = ''
 
       if (data['data']) {
         data['data'].forEach((user) => {
           if (user.situacionlab && user.situacionlab === '1') {
-            disponibilidad = 'Disponible';
-            classAvailable = 'tag-purple';
+            disponibilidad = 'Disponible'
+            classAvailable = 'tag-purple'
           } else {
-            disponibilidad = 'No Disponible';
-            classNoAvailable = 'tag-red';
+            disponibilidad = 'No Disponible'
+            classNoAvailable = 'tag-red'
           }
 
           if (user.genero === '1') {
-            gender = 'Hombre';
+            gender = 'Hombre'
           } else if (user.genero === '2') {
-            gender = 'Mujer';
+            gender = 'Mujer'
           } else if (user.genero === '3') {
-            gender = 'No Binarix';
+            gender = 'No Binarix'
           } else {
-            gender = 'Otro';
+            gender = 'Otro'
           }
 
-          user.modalidad === '1'
-            ? (tipoModalidad = 'Full-Time')
-            : tipoModalidad;
+          user.modalidad === '1' ? (tipoModalidad = 'Full-Time') : tipoModalidad
 
-          user.modalidad === '2'
-            ? (tipoModalidad = 'Part-Time')
-            : tipoModalidad;
+          user.modalidad === '2' ? (tipoModalidad = 'Part-Time') : tipoModalidad
 
-          user.modalidad === '3' ? (tipoModalidad = 'Trainee') : tipoModalidad;
+          user.modalidad === '3' ? (tipoModalidad = 'Trainee') : tipoModalidad
 
-          user.modalidad === '4'
-            ? (tipoModalidad = 'Pasantías')
-            : tipoModalidad;
+          user.modalidad === '4' ? (tipoModalidad = 'Pasantías') : tipoModalidad
 
           if (user.foto) {
-            foto = '../db/images/' + user.foto;
+            foto = '../db/images/' + user.foto
           } else {
-            foto = '../assets/logo.jpg';
+            foto = '../assets/logo.jpg'
           }
           $('.div-datos').append(
             `<a href="../vistas/vistaUsuario.php?iduser=${user.iduser}" target="_blank" class="card">
@@ -226,23 +215,23 @@ const sendRequest = () => {
                   </div>
               </div>
           </a>`
-          );
-          classAvailable = '';
-        });
+          )
+          classAvailable = ''
+        })
       } else {
-        $('.div-datos').empty();
+        $('.div-datos').empty()
         $('.div-datos').append(`
           <div class="errorMessage">No se encontraron Resultados</div>
-        `);
+        `)
       }
     },
     error: (err) => {
-      console.log(err);
+      console.log(err)
     },
-  });
-};
+  })
+}
 
 // Retorna el genero con la primera letra en Mayuscula
 const capitalizarPrimeraLetra = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
